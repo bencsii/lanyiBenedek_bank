08bef0edac700322afa1f1daef36cbd85630e08d
Petrik Diak <diak@petrik.hu>
2025-03-28 12:42:55 +0100

    BankAccount class kesz

---
+++ abstract_bank_uj/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_bank_uj
+{
+	internal abstract class BankAccount : BankingService
+	{
+		public double Balance { get; protected set; }
+
+		protected BankAccount(Owner owner) : base(owner)
+		{
+
+		}
+
+		public void Deposit(double amount)
+		{
+			Balance += amount;
+
+		}
+
+		public abstract bool Withdraw(double amount);
+
+		public BankCard NewCard(string cardNumber)
+		{
+			return new BankCard(Owner, Balance, cardNumber);
+		}
+	}
+}
