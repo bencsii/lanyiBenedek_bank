75603d8e73db79e11c4b70a5b9ce776218cc158d
Petrik Diak <diak@petrik.hu>
2025-03-28 12:32:56 +0100

    Owner class kesz

---
+++ abstract_bank_uj/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+	internal sealed class Owner
+	{
+		public string Owners_Name { get; set; }
+
+		
+	}
+}
