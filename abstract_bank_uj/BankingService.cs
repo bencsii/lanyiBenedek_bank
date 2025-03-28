6283e81c451238336dc7791eb020f868fd23f6a5
Petrik Diak <diak@petrik.hu>
2025-03-28 12:37:53 +0100

    Banking service class kesz

---
+++ abstract_bank_uj/BankingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_bank_uj
+{
+	internal abstract class BankingService
+	{
+		Owner owner;
+		public Owner Owner { get => owner; }
+
+		public BankingService(Owner owner) { this.owner = owner; }
+	}
+}
--- abstract_bank_uj/Owner.cs
+++ abstract_bank_uj/Owner.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bank
+namespace abstract_bank_uj
 {
 	internal sealed class Owner
 	{
