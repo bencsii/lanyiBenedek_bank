f7b829a116ced21beef651ff077136b85998e123
Petrik Diak <diak@petrik.hu>
2025-03-28 12:47:23 +0100

    CreditAccount class kesz

---
+++ abstract_bank_uj/CreditAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_bank_uj
+{
+	internal class CreditAccount : BankAccount
+	{
+		double creditLimit;
+		public double Creditlimit { get; }
+
+		public CreditAccount(Owner owner, double creditlimit) : base(owner)
+		{
+			this.creditlimit = creditlimit;
+		}
+
+		public override bool Withdraw(double amount)
+		{
+			if (this.Balance - amount >= creditlimit)
+			{
+				this.Balance -= amount;
+				return true;
+			}
+			return false;
+		}
+	}
+}
