9390b3d637b603071b0748125fc8ca8b7ca55f06
bencsii <benicke001@gmail.com>
2025-04-03 19:40:35 +0200

    BankAccount class kesz

---
+++ bankos_ottohn/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankos_ottohn
+{
+    internal abstract class BankAccount : BankingService
+    {
+        private double balance;
+
+
+        public double Balance { get => balance; set => balance = value; }
+
+        protected BankAccount(Owner owner) : base(owner)
+        {
+
+        }
+
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+
+        }
+
+        public abstract bool Withdraw(double amount);
+    }
+}
