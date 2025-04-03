578244b25c1451fd44c89b1d0240d79bd0bb0f4c
bencsii <benicke001@gmail.com>
2025-04-03 19:30:06 +0200

    owner class kesz

---
+++ bankos_ottohn/Owner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankos_ottohn
+{
+    internal sealed class Owner
+    {
+        public string ownerName { get; set; }
+    }
+}
