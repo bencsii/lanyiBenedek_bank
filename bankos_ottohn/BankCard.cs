f516983b1bb79abd5415aa8c6a39224898bc13b6
bencsii <benicke001@gmail.com>
2025-04-03 20:15:33 +0200

    BankCard class kesz

---
+++ bankos_ottohn/BankCard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankos_ottohn
+{
+    internal class BankCard
+    {
+        int cardNumber;
+        double accountBalance;
+
+        public double AccountBalance { get => accountBalance;}
+        public int CardNumber { get => cardNumber;}
+
+        public BankCard(Owner owner, double accountBalance, int cardNumber)
+        {
+            this.accountBalance = accountBalance;
+            this.cardNumber = cardNumber;
+        }
+
+        public bool Purchase(double amount)
+        {
+            if (accountBalance - amount >= 0)
+            {
+                accountBalance -= amount;
+                return true;
+            }
+            return false;
+        }
+    }
+}
