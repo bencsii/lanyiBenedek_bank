6764245306739efe5c51743d0b0e3398e80c763b
bencsii <benicke001@gmail.com>
2025-04-03 19:52:08 +0200

    CreditAccount class kesz

---
+++ bankos_ottohn/CreditAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankos_ottohn
+{
+    internal class CreditAccount : BankAccount
+    {
+        double creditLimit;
+        public double Creditlimit { get => creditLimit;}
+
+        public CreditAccount(Owner owner, double creditlimit) : base(owner)
+        {
+            this.creditLimit = creditlimit;
+        }
+
+        public override bool Withdraw(double amount)
+        {
+            if (this.Balance - amount >= creditLimit)
+            {
+                this.Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
